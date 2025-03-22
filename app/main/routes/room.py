39d580f0b59fad48e5d26f6c77bd0dd1a8263d79
Filipe Mai <f.nunesmai19@gmail.com>
2025-03-22 10:37:28 -0300

    Correção da definicao das rotas

--- app/main/routes/room.py
+++ app/main/routes/room.py
@@ -1,45 +1,46 @@
-from fastapi import Depends
+from fastapi import Depends, APIRouter
 from fastapi.responses import JSONResponse
 
-from ..server.server import app
-
 # Importação o gerente de conexões
-from app.main.models.settings.db_communication import db_connection_handler
+from main.models.settings.db_communication import db_connection_handler
+
+# Criação do APIRouter
+router = APIRouter()
 
 # Importação do Repositorio
-from app.main.models.repositories.room_repository import RoomRepository
+from main.models.repositories.room_repository import RoomRepository
 
 # Importação dos Controllers
-from app.main.controllers.room_creator import RoomCreator
-from app.main.controllers.room_finder import RoomFinder 
-
+from main.controllers.room_creator import RoomCreator
+from main.controllers.room_finder import RoomFinder 
+print("dfhj")
 def get_room_repository():
     conn = db_connection_handler.get_connection()
     return RoomRepository(conn)
 
-@app.get("/hi/{who}")
+@router.get("/hi/{who}")
 def hello(who):
     return f"Hello? {who}?"
 
-@app.get("/rooms")
+@router.get("/rooms")
 def get_rooms(room_repository: RoomRepository = Depends(get_room_repository)):
     controller = RoomFinder(room_repository)
     response = controller.find_all()
     return JSONResponse(response['body']), response['status_code']
 
-@app.get("/rooms/{id}")
+@router.get("/rooms/{id}")
 def get_rooms(room_id: str, room_repository: RoomRepository = Depends(get_room_repository)):
     controller = RoomFinder(room_repository)
     response = controller.find_one(room_id=room_id)
     return JSONResponse(response['body']), response['status_code']
 
-@app.post("/rooms")
+@router.post("/rooms")
 def create_room(room_repository: RoomRepository = Depends(get_room_repository)):
     controller = RoomCreator(room_repository)
     response = controller.create()
     return JSONResponse(response['body']), response['status_code']
 
-@app.delete("/rooms/{id}")
+@router.delete("/rooms/{id}")
 def delete_rooms(room_id: str, room_repository: RoomRepository = Depends(get_room_repository)):
     controller = RoomCreator(room_repository)
     response = controller.delete(room_id=room_id)
